7624ea5000e6d8ea659c9bb6a6e3b96805a2e6a4
agungkrx <47ungkrs@gmail.com>
2026-07-05 19:19:20 +0800

    add contact section

--- src/components/sections/contact/Contact.tsx
+++ src/components/sections/contact/Contact.tsx
@@ -1,3 +1,45 @@
+import Image from 'next/image';
+import { contact } from '@/data/contact';
+import styles from './Contact.module.css';
+
 export default function Contact() {
-  return <h1>contact</h1>;
+  return (
+    <section id="contact">
+      <div className={`maxwidth ${styles.container}`}>
+        <div>
+          <Image
+            src="/images/stay-in-touch-base.png"
+            alt="Stay in touch"
+            width={147}
+            height={103}
+            style={{ imageRendering: 'pixelated' }}
+            className={`${styles.sty}`}
+          />
+        </div>
+        <div className={`${styles.dialogbox}`}>
+          <Image
+            src="/images/me-phone-o.png"
+            alt="Pixel Art Sprite"
+            width={186}
+            height={238}
+            style={{ imageRendering: 'pixelated' }}
+          />
+          <p>{contact.summary}</p>
+        </div>
+        <ul className={`${styles.link}`}>
+          {contact.links.map((link) => (
+            <li key={link.name}>
+              <Image
+                src={link.icon}
+                alt={`Sprite ${link.name}`}
+                width={25}
+                height={23}
+              />
+              <a href="#">{link.term}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
+  );
 }
---
+++ src/data/contact.ts
@@ -0,0 +1,16 @@
+export const contact = {
+  summary:
+    'Currently open to new opportunities, freelance projects, and full-time positions. Feel free to get in touch anytime.',
+  links: [
+    {
+      name: 'Email',
+      term: 'Email is my preferred way to communicate.',
+      icon: '/icon/email.png',
+    },
+    {
+      name: 'Linked In',
+      term: 'You can also reach me on LinkedIn.',
+      icon: '/icon/linkedin.png',
+    },
+  ],
+};
