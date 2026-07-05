8264ed8e36e5673b6e1403794d6d89cfc1702960
agungkrx <47ungkrs@gmail.com>
2026-07-05 23:59:12 +0800

    add toast notification

--- src/components/client/contacts/ContactLink.tsx
+++ src/components/client/contacts/ContactLink.tsx
@@ -1,7 +1,15 @@
 'use client';
 
+import styles from './ContactLink.module.css';
 import { copyToClipBoard } from '@/utils/clipboard';
 import Image from 'next/image';
+import { useState } from 'react';
+
+type Toast = {
+  id: string;
+  message: string;
+  type: 'success' | 'error';
+};
 
 type Links = {
   url: string;
@@ -15,45 +23,85 @@ type Props = {
 };
 
 export function ContactLink({ links, styles }: Props) {
+  const [toast, setToast] = useState<Toast[]>([]);
+  const [counter, setCounter] = useState<number>(0);
+
   function handleCopied(str: string) {
     const email = str.match(/^mailto:(.+)$/)?.[1];
 
-    if (!email) return;
-
+    if (!email) {
+      showToast('Failed to copy!', 'error');
+      return;
+    }
     copyToClipBoard(email);
-    
+    showToast(`Copied: ${email}`, 'success');
+  }
+
+  function showToast(message: string, type: Toast['type']) {
+    setCounter((prev) => prev + 1);
+    const id = String(counter);
+
+    setToast((prev) => [...prev, { id, message, type }]);
+
+    setTimeout(() => {
+      removeToast(id);
+    }, 3000);
+  }
+
+  function removeToast(id: string) {
+    setToast((prev) => prev.filter((i) => i.id !== id));
   }
   return (
-    <ul className={`${styles}`}>
-      {links.map((link) => {
-        const isExternal =
-          link.url.startsWith('http://') || link.url.startsWith('https://');
+    <>
+      {toast.map((t) => (
+        <ToastContainer key={t.id} type={t.type} message={t.message} />
+      ))}
+      <ul className={`${styles}`}>
+        {links.map((link) => {
+          const isExternal =
+            link.url.startsWith('http://') || link.url.startsWith('https://');
 
-        const anchorProps = isExternal
-          ? {
-              href: link.url,
-              target: '_blank',
-              rel: 'noopener noreferrer',
-            }
-          : {
-              href: link.url,
-              onClick: () => handleCopied(link.url),
-            };
+          const anchorProps = isExternal
+            ? {
+                href: link.url,
+                target: '_blank',
+                rel: 'noopener noreferrer',
+              }
+            : {
+                href: link.url,
+                onClick: () => handleCopied(link.url),
+              };
 
-        return (
-          <li key={link.name}>
-            <a {...anchorProps}>
-              <Image
-                src={link.icon}
-                alt={`Sprite ${link.name}`}
-                width={25}
-                height={23}
-              />
-              {link.term}
-            </a>
-          </li>
-        );
-      })}
-    </ul>
+          return (
+            <li key={link.name}>
+              <a {...anchorProps}>
+                <Image
+                  src={link.icon}
+                  alt={`Sprite ${link.name}`}
+                  width={25}
+                  height={23}
+                />
+                {link.term}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+}
+
+function ToastContainer({ type, message }: { type: string; message: string }) {
+  console.log(type);
+  return (
+    <div className={styles.toastContainer}>
+      <Image
+        src="/icon/toast-cat.png"
+        alt="sprite-cat"
+        width={25}
+        height={31}
+      />
+      <span>{message}</span>
+    </div>
   );
 }
