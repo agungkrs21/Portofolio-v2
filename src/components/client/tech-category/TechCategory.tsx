93696cd44f96ac5d7f115cdd6c118b6787c3d804
agungkrx <47ungkrs@gmail.com>
2026-07-08 00:34:45 +0800

    feat: add interactive teachstack filtering with animations

--- src/components/client/tech-category/TechCategory.tsx
+++ src/components/client/tech-category/TechCategory.tsx
@@ -34,38 +34,63 @@ interface CategoriesProps {
 }
 
 function Categories({ categories, techstacks }: CategoriesProps) {
-  const [filtered, setFiltered] = useState<TechStacks[]>(techstacks);
+  const [fitler, setFilter] = useState<string>('All');
+  const [animate, setAnimate] = useState<boolean>(false);
+
+  const filteredTech =
+    fitler === 'All'
+      ? techstacks
+      : techstacks.filter((tech) => tech.category === fitler);
+
+  function handlerFilter(category: string) {
+    setFilter(category);
+    setAnimate(false);
+
+    requestAnimationFrame(() => {
+      setAnimate(true);
+    });
+  }
 
   return (
     <>
       <ul className={`${styles.categories}`}>
         <li>
-          <Image
-            src="/icon/all.png"
-            alt=""
-            width={21}
-            height={21}
-            className={`rendring-pixelated ${styles.ctIcon}`}
-          />
-          <p>All</p>
-        </li>
-        {categories.map((category) => (
-          <li key={category}>
+          <button
+            className={`${fitler === 'all' ? styles.ctButtonActive : styles.ctButtonDeactive}`}
+            onClick={() => handlerFilter('All')}
+          >
             <Image
-              src={`/icon/${category.toLowerCase()}.png`}
+              src="/icon/all.png"
               alt=""
               width={21}
               height={21}
               className={`rendring-pixelated ${styles.ctIcon}`}
             />
-            <p>{category}</p>
+            <p>All</p>
+          </button>
+        </li>
+        {categories.map((category) => (
+          <li key={category}>
+            <button
+              className={`${fitler === category ? styles.ctButtonActive : styles.ctButtonDeactive}`}
+              onClick={() => handlerFilter(category)}
+            >
+              <Image
+                src={`/icon/${category.toLowerCase()}.png`}
+                alt=""
+                width={21}
+                height={21}
+                className={`rendring-pixelated ${styles.ctIcon}`}
+              />
+              <p>{category}</p>
+            </button>
           </li>
         ))}
       </ul>
 
-      <div className={`${styles.itemsContainer}`}>
-        {filtered.map((tech) => (
-          <Items key={tech.category} techs={tech} />
+      <div className={`${styles.itemsContainer}  `}>
+        {filteredTech.map((tech) => (
+          <Items key={tech.category} techs={tech} animate={animate} />
         ))}
       </div>
     </>
@@ -74,22 +99,23 @@ function Categories({ categories, techstacks }: CategoriesProps) {
 
 interface ItemsProps {
   techs: TechStacks;
+  animate: boolean;
 }
 
-function Items({ techs }: ItemsProps) {
+function Items({ techs, animate }: ItemsProps) {
   return (
     <section className={`${styles.items}`}>
       <h6>{techs.category}</h6>
       <p>{techs.summary}</p>
       <ul>
         {techs.tech.map((t) => (
-          <li key={t.name}>
+          <li key={t.name} className={`${animate ? styles.animateFedeIn : ''}`}>
             <Image
               src={`${t.icon}`}
               alt="Sprite React"
               width={63}
               height={58}
-              className={`rendering-pixelated ${styles.itemIcon} ${styles.gray}`}
+              className={`rendering-pixelated ${styles.itemIcon} ${styles.gray} `}
             />
             <p>{t.name}</p>
           </li>
