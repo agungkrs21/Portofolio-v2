5ea0c22470af9ce5a1b740dbaa4e03decb4d1936
agungkrx <47ungkrs@gmail.com>
2026-07-08 11:37:34 +0800

    feat: add timeline to about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,76 @@
+import Image from 'next/image';
+import { lifeStory } from '@/data/profile';
+import styles from './Page.module.css';
+import { timelines } from '@/data/profile';
+
 export default function About() {
   return (
-    <section>
-      <div className="firstPage maxwidth">
-        <h1>Abot me here</h1>
+    <section aria-labelledby="about-heding">
+      <div className={`firstPage maxwidth ${styles.container}`}>
+        <AboutDetail />
+        <Timeline timelines={timelines} />
       </div>
+    </section>
+  );
+}
+
+function AboutDetail() {
+  return (
+    <div className={`${styles.about}`}>
+      <div className={`${styles.catbg}`}></div>
+      <div>
+        <h1 id="about-heading" className={`${styles.title}`}>
+          About
+        </h1>
+        {lifeStory.map((story, i) => (
+          <p key={i}>{story}</p>
+        ))}
+      </div>
+      <div className={`${styles.imageCt}`}>
+        <Image
+          src="/images/me-1000.png"
+          alt="Agung"
+          width={500}
+          height={500}
+          className={`${styles.profilePic}`}
+        />
+      </div>
+    </div>
+  );
+}
+
+interface TimelineProps {
+  timelines: {
+    time: string;
+    title: string;
+    summary: string;
+  }[];
+}
+
+function Timeline({ timelines }: TimelineProps) {
+  return (
+    <section
+      aria-labelledby="timeline-heading"
+      className={`${styles.timelineCt}`}
+    >
+      <h2 id="timeline-heading" className={`${styles.title}`}>
+        Timeline
+      </h2>
+      <p>A short timeline of how I became a Frontend Developer</p>
+
+      <ol>
+        {timelines.map((tl) => (
+          <li key={tl.time} className={`${styles.timeline}`}>
+            <div>
+              <time dateTime={tl.time}>📅 {tl.time}</time>
+              <h3>{tl.title}</h3>
+            </div>
+            <div>
+              <p>{tl.summary}</p>
+            </div>
+          </li>
+        ))}
+      </ol>
     </section>
   );
 }
--- src/data/profile.ts
+++ src/data/profile.ts
@@ -20,9 +20,45 @@ export const profile = {
 export const lifeStory = [
   "Hi there! I'm Agung Kurniawan, and I enjoy turning ideas into reality—not because I'm some kind of magician, but because I'm a Front-End Developer. Building things is what keeps me motivated. I love challenging myself, learning through every project, and taking pride in creating products that are both functional and enjoyable to use.",
 
-  "My interest in technology started with video games. What began as curiosity about how games and software worked eventually led me to programming. I studied Computer and Network Engineering in vocational high school before earning a Bachelor's degree in Computer Science. Along the way, I co-founded a small technology community with friends, served as an assistant instructor, and later worked as a programming laboratory assistant, where I helped students understand programming concepts and solve technical problems.",
-
   "Currently, I'm focused on growing as a web developer by building personal projects that push me to learn something new. I enjoy creating applications with modern web technologies while exploring interactive experiences and creative design.",
 
   "When I'm away from the keyboard, you'll usually find me playing video games, listening to music, exploring new technologies, or experimenting with ideas that eventually become my next side project.",
+];
+
+export const timelines = [
+  {
+    time: 'Today',
+    title: 'Fronend Developer',
+    summary: 'Building interactive web applications.',
+  },
+  {
+    time: '2025',
+    title: 'Landed My First Commissioned Project',
+    summary:
+      "This was a significant milestone in my journey. While developing the Kalosi Health Center Information System, I graduated with a Bachelor's degree in Computer Science. During this period, I also earned two additional commissioned projects through recommendations from my university peers, allowing me to gain more real-world development experience.",
+  },
+  {
+    time: '2024',
+    title: 'IT Engineer (Contract)',
+    summary:
+      'Recommended by a senior at my university to join a project team as a Contract IT Engineer, where I helped support a large-scale Linux operating system migration at Pegadaian.',
+  },
+  {
+    time: '2023',
+    title: 'Build My First Website',
+    summary:
+      'During this time, I explored several programming languages before eventually discovering JavaScript. I was drawn to its simplicity and ease of understanding, but what truly sparked my interest was the free book Eloquent JavaScript. Its clear explanations and engaging style made learning enjoyable, deepened my appreciation for programming, and inspired me to build my first website.',
+  },
+  {
+    time: '2018',
+    title: 'Create A small comunity',
+    summary:
+      'Founded a small community called Broinet in my hometown to introduce people to computer networking. Through this initiative, we also had the opportunity to design and set up local area networks (LANs) for several schools in the area.',
+  },
+  {
+    time: '2015',
+    title: 'Computer & Network Engineering',
+    summary:
+      'This is where it all began. I enrolled in a vocational high school and majored in Computer and Network Engineering.',
+  },
 ];
