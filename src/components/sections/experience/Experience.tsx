6029ffd0d3917c0d809bcd5a301a56f519bdf823
agungkrx <47ungkrs@gmail.com>
2026-07-09 04:52:54 +0800

    feat: add ts linux category in experience section

--- src/components/sections/experience/Experience.tsx
+++ src/components/sections/experience/Experience.tsx
@@ -2,8 +2,7 @@ import { profile } from '@/data/profile';
 import styles from './Experience.module.css';
 import { TechCategory } from '@/components/client/tech-category/TechCategory';
 import { techstacks } from '@/data/techstack';
-
-
+import Image from 'next/image';
 
 export default function Experince() {
   return (
@@ -12,7 +11,41 @@ export default function Experince() {
         <p>{profile.about}</p>
         <p>Some technologies I&apos;ve worked with:</p>
 
-        <TechCategory techstacks={techstacks}  />
+        <TechCategory techstacks={techstacks} />
+      </div>
+      <div className={` maxwidth ${styles.scriptCT}`}>
+        <div>
+          <h3>TypeScript</h3>
+          <p>
+            I am proficient in TypeScript, whether I&apos;m working with React
+            or Node.js.
+          </p>
+
+          <Image
+            src="/images/tstext-logo.png"
+            alt=""
+            width={300}
+            height={120}
+            className={`[image-rendering:pixelated] ${styles.gray}`}
+            unoptimized
+          />
+        </div>
+        <div>
+          <h3>Arch Linux</h3>
+          <p>
+            I am proficient in TypeScript, whether I&apos;m working with React
+            or Node.js.
+          </p>
+
+          <Image
+            src="/images/archtext-logo.png"
+            alt=""
+            width={300}
+            height={120}
+            className={`[image-rendering:pixelated] ${styles.gray}`}
+            unoptimized
+          />
+        </div>
       </div>
     </section>
   );
