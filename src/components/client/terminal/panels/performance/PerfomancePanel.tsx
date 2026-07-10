18440419830f1763619e4963882a3e7cf5fccdc5
agungkrx <47ungkrs@gmail.com>
2026-07-10 08:18:02 +0800

    style: add styling to Perfomance component

--- src/components/client/terminal/panels/performance/PerfomancePanel.tsx
+++ src/components/client/terminal/panels/performance/PerfomancePanel.tsx
@@ -1,3 +1,60 @@
+import styles from './PerfomancePanel.module.css';
+
 export default function PerfomancePanel() {
-  return <p>Performance Panel</p>;
+  return (
+    <div className={`${styles.container}`}>
+      <p>Real User Metrics</p>
+      <div>
+        <p>LCP</p>
+        <p>INP</p>
+        <p>CLS</p>
+        <p>FCP</p>
+        <p>TTFB</p>
+      </div>
+      <div>
+        <p>1.42 s</p>
+        <p>97 ms</p>
+        <p>0.03 </p>
+        <p>856 ms</p>
+        <p>184 ms</p>
+      </div>
+      <div>
+        <p>
+          <span
+            className={`${styles.circle}`}
+            style={{ background: 'var(--green)' }}
+          ></span>{' '}
+          Good
+        </p>
+        <p>
+          <span
+            className={`${styles.circle}`}
+            style={{ background: 'var(--green)' }}
+          ></span>{' '}
+          Good
+        </p>
+        <p>
+          <span
+            className={`${styles.circle}`}
+            style={{ background: 'var(--green)' }}
+          ></span>{' '}
+          Good
+        </p>
+        <p>
+          <span
+            className={`${styles.circle}`}
+            style={{ background: 'var(--green)' }}
+          ></span>{' '}
+          Good
+        </p>
+        <p>
+          <span
+            className={`${styles.circle}`}
+            style={{ background: 'var(--green)' }}
+          ></span>{' '}
+          Good
+        </p>
+      </div>
+    </div>
+  );
 }
