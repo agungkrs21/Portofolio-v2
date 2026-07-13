12b1cd98d2f6a5334a1552d7457d3cdfa3b8d440
agungkrx <47ungkrs@gmail.com>
2026-07-13 10:18:03 +0800

    feat: add recent_commits url, add get function for recent commit

--- src/lib/github/read-recent-commits.ts
+++ src/lib/github/read-recent-commits.ts
@@ -2,16 +2,37 @@ import { RecentCommit } from '@/lib/github/types';
 import { readFile } from 'fs/promises';
 import path from 'path';
 
+// TODO useCache, and refractor code
 export async function readRecentCommits(): Promise<RecentCommit[]> {
   try {
-    const filePath = path.join(process.cwd(), 'data', 'recent-commits.json');
-
-    const json = await readFile(filePath, 'utf8');
-
-    return JSON.parse(json) as RecentCommit[];
+    return await read();
   } catch (error) {
     throw new Error('Failed to read recent commits.', {
       cause: error,
     });
   }
+}
+
+async function read() {
+  const filePath = path.join(process.cwd(), 'data', 'recent-commits.json');
+
+  const json = await readFile(filePath, 'utf8');
+
+  return JSON.parse(json) as RecentCommit[];
+}
+
+async function get() {
+  const url = process.env.RECENT_COMMIT_URL;
+
+  if (!url) {
+    throw new Error('RECENT_COMMIT_URL is not configured.');
+  }
+
+  const res = await fetch(url);
+
+  if (!res.ok) {
+    throw new Error('Failed to get recent commits');
+  }
+  const data = await res.json();
+  return data as RecentCommit[];
 }
