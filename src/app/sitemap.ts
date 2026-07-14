343fd5e6943ae6143ad0949e7bfa31b286a62b61
agungkrx <47ungkrs@gmail.com>
2026-07-14 15:48:42 +0800

    feat: add robots.ts and sitemap.ts

---
+++ src/app/sitemap.ts
@@ -0,0 +1,23 @@
+import { siteConfig } from '@/config/site';
+import type { MetadataRoute } from 'next';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: `${siteConfig.url}/en`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${siteConfig.url}/id`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${siteConfig.url}/en/about`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${siteConfig.url}/en/about`,
+      lastModified: new Date(),
+    },
+  ];
+}
